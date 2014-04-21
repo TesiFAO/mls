c706e1c612ff37b4900a3672836656af22229b2d
Kalimaha <GeoBricks2012>
2014-04-21 12:19:36 +0200

    Ricerca del massimo periodo al variare di m

--- src/test/java/it/unimarconi/mls/GeneratoreMoltiplicativoTest.java
+++ src/test/java/it/unimarconi/mls/GeneratoreMoltiplicativoTest.java
@@ -6,12 +6,27 @@ import java.util.List;
 
 public class GeneratoreMoltiplicativoTest extends TestCase {
 
+    /**
+     * Ricerca del massimo periodo al variare di m
+     */
     public void testGenerate() {
+        Integer max = 0;
+        Integer max_m = 0;
         Integer a = 11;
         Integer x0 = 3;
-        Integer m = 32;
-        List<Integer> l = GeneratoreMoltiplicativo.generate(a, x0, m);
-        System.out.println("[a = " + a + "][x = " + x0 + "][m = " + m + "] | Periodo: " + l.size());
+        for (int m = 1 ; m < 1024 ; m++) {
+            List<Integer> l = GeneratoreMoltiplicativo.generate(a, x0, m);
+            if (max < l.size()) {
+                max = l.size();
+                max_m = m;
+            }
+        }
+        System.out.println("Periodo massimo " + max + " ottenuto per m = " + max_m);
+        List<Integer> lf = GeneratoreMoltiplicativo.generate(a, x0, max_m);
+        System.out.println("[a = " + a + "][x = " + x0 + "][m = " + max_m + "] | Periodo: " + lf.size());
+        for (int j = 0; j < lf.size(); j++)
+            System.out.println(j + ") " + lf.get(j));
+        System.out.println("\n");
     }
 
     /**

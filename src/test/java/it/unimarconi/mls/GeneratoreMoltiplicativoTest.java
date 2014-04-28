8388926e559b843943abc1659e9408e6341e7b97
Kalimaha <guido.barbaglia@fao.org>
2014-04-28 15:50:29 +0200

    asd

--- src/test/java/it/unimarconi/mls/GeneratoreMoltiplicativoTest.java
+++ src/test/java/it/unimarconi/mls/GeneratoreMoltiplicativoTest.java
@@ -1,11 +1,7 @@
 package it.unimarconi.mls;
 
 import junit.framework.TestCase;
-import org.apache.commons.math.linear.RealMatrix;
-import org.apache.commons.math.stat.correlation.Covariance;
-import org.apache.commons.math.stat.correlation.PearsonsCorrelation;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GeneratoreMoltiplicativoTest extends TestCase {
@@ -43,8 +39,8 @@ public class GeneratoreMoltiplicativoTest extends TestCase {
         Integer differentLists = 1;
         for (int i = 0 ; i < as.size() ; i++) {
             Integer a = as.get(i);
-            Integer x0 = 3;
-            Integer b = 5;
+            Integer x0 = 1;
+            Integer b = 6;
             Integer m = new Double(Math.pow(2, b)).intValue();
             List<Integer> l = GeneratoreMoltiplicativo.generate(a, x0, m);
             if (memory == null)
@@ -67,7 +63,7 @@ public class GeneratoreMoltiplicativoTest extends TestCase {
     public void testGenerateWithXs() {
         Integer a = GeneratoreMoltiplicativo.generateA(8, 5, 1).get(0);
         Integer b = 6;
-        Integer m = 29;
+        Integer m = new Double(Math.pow(2, b)).intValue();
         List<Integer> xs = GeneratoreMoltiplicativo.generateX(m);
         System.out.println(xs.size() + " values for x");
         List<Integer> memory = null;
@@ -96,98 +92,6 @@ public class GeneratoreMoltiplicativoTest extends TestCase {
         Integer m = 8;
         Integer q = 5;
         List<Integer> l = GeneratoreMoltiplicativo.generateA(m, q, 10);
-    }
-
-    public void testCov() {
-
-        /* Initiate data matrix. */
-        List<List<Integer>> data = new ArrayList<List<Integer>>();
-
-        /* Initiate parameters. */
-        List<Integer> as = GeneratoreMoltiplicativo.generateA(8, 5, 20);
-        Integer b = 5;
-        Integer m = new Double(Math.pow(2, b)).intValue();
-        Integer x0 = 3;
-
-        /* Generate sequences changing the value of parameter a. */
-        for (int i = 0 ; i < as.size() ; i++) {
-            Integer a = as.get(i);
-            List<Integer> l = GeneratoreMoltiplicativo.generate(a, x0, m);
-            data.add(l);
-        }
-
-        /* Iterate over the lists to calculate the covariance values. */
-        for (int i = 0 ; i < data.size() - 1 ; i++) {
-            double one[] = list2array(data.get(i));
-            double two[] = list2array(data.get(i + 1));
-            double cov = new Covariance().covariance(one, two);
-            double cor = new PearsonsCorrelation().correlation(one, two);
-            System.out.println(cor + " | " + cov);
-        }
-
-    }
-
-    public double[] list2array(List<Integer> l) {
-        double a[] = new double[l.size()];
-        for (int i = 0 ; i < l.size() ; i++)
-            a[i] = l.get(i);
-        return a;
-    }
-
-    public void testRange() {
-        List<Integer> ts = new ArrayList<Integer>();
-        double lambda = 0.05;
-        double min = 30;
-        double max = 50;
-        for (int i = 0 ; i < 100 ; i++) {
-            double rand = 1 - Math.exp(-lambda * i);
-            System.out.print((min + rand * ((max - min))) + ", ");
-        }
-    }
-
-    public void testGenerateExponential() {
-        double lambda = 0.05;
-        for (int i = 0 ; i < 100 ; i++) {
-//            double rand = 1 - Math.exp(-lambda * i);
-//            System.out.print((-1 * (1 / lambda) * Math.log(rand)) + ", ");
-            System.out.println(generateExponential(lambda, i));
-        }
-    }
-
-    public void testGenerateIperexponential() {
-        double l = 1.0 / 20.0;
-        double p = 0.38;
-        double sum = 0;
-//        double l1 = 2 * p * l;
-//        double l2 = 2 * (1 - p) * l;
-        double Ta = 1 / l;
-        String s = "i = c(";
-        for (double t = 1 ; t < 10 ; t += 0.1) {
-            double Z = generateHyperexponential(l, p, t);
-            s += Z;
-            sum += Z / 100;
-            if (t < 99)
-                s += ", ";
-        }
-        s += "); plot(i);";
-//        System.out.println(s);
-//        System.out.println(sum);
-    }
-
-    public double generateHyperexponential(double lambda, double p, double t) {
-        double Ta = 1 / lambda;
-        double r = (1 - p) * Math.exp(-2 * p * lambda * t) - ((1 - p) * Math.exp(-2 * (1 - p) * lambda * t));
-        double X = generateExponential(lambda, t);
-        if (r <= p)
-            return (Ta / (2 * p)) * X;
-        else
-            return (Ta / (2 * (1 - p))) * X;
-    }
-
-    public double generateExponential(double lambda, double t) {
-        double rand = 1 - Math.exp(-lambda * t);
-        System.out.print(-2 * Math.log(rand) + ", ");
-        return -1 * (1 / lambda) * Math.log(rand);
     }
 
 }
